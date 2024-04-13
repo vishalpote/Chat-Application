911ef97af416fc7ac497d58233991e2f9ecef873
vishal <vishalpote9090@gmail.com>
2024-04-13 17:48:40 +0530

    Change DB Connection

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
-// const database = process.env.MONGO_URI;
+const database = process.env.MONGO_URI;
 export const connection=async()=>{
     try {
-        await mongoose.connect(
-          `mongodb+srv://vishal:vishalpote@cluster0.ljkbz4k.mongodb.net/?retryWrites=true&w=majority`
-        );
+        await mongoose.connect(database);
         console.log("MongoDB connection established..!!");
     } catch (error) {
-        console.log("Error While Connecting MongoDB...!!");
+        console.log("Error While Connecting MongoDB...!!"+error.message);
     }
 }
