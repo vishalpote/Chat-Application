1af5561670f03b17ffe91ce5e59d2c2595b4b66a
vishal <vishalpote9090@gmail.com>
2024-04-13 17:56:13 +0530

    Change DB Connection

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
-const database = process.env.MONGO_URI;
+// const database = process.env.MONGO_URI;
+const database ="mongodb+srv://vishal:vishalpote@cluster0.ljkbz4k.mongodb.net/?retryWrites=true&w=majority";
+// console.log(database);
 export const connection=async()=>{
     try {
         await mongoose.connect(database);
